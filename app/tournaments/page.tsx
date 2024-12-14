83b6acf44280a255e1f6a6ebd1a789707bfeb735
Albert Abzalimov <sickloljuly@gmail.com>
2024-12-13 20:04:19 -0500

    added bracket library, added placeholder messages and standings

--- app/tournaments/page.tsx
+++ app/tournaments/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
     return (
@@ -11,7 +12,7 @@ export default function Home() {
                         width={500}
                         height={400}
                         alt="Picture of the author"
-                        className='w-[70%]'
+                        className="w-[70%]"
                     />
                     <div className="px-6 py-4 text-center">
                         <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
@@ -19,12 +20,12 @@ export default function Home() {
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                         </p>
                     </div>
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#382460] rounded-lg hover:bg-[#261843] focus:ring-4 focus:outline-none focus:ring-[#412a6e]">
+                    <Link href="/tournament/manage/1" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#382460] rounded-lg hover:bg-[#261843] focus:ring-4 focus:outline-none focus:ring-[#412a6e]">
                         Read more
                         <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                         </svg>
-                    </a>
+                    </Link>
                 </div>
             </div>
             <h1 className="text-[#7458da] font-bold text-3xl ml-20 mt-5">Previous Tournaments</h1>
@@ -35,7 +36,7 @@ export default function Home() {
                         width={500}
                         height={400}
                         alt="Picture of the author"
-                        className='w-[70%]'
+                        className="w-[70%]"
                     />
                     <div className="px-6 py-4 text-center">
                         <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
@@ -43,14 +44,14 @@ export default function Home() {
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                         </p>
                     </div>
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#382460] rounded-lg hover:bg-[#261843] focus:ring-4 focus:outline-none focus:ring-[#412a6e]">
+                    <Link href="/tournament/manage/1" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#382460] rounded-lg hover:bg-[#261843] focus:ring-4 focus:outline-none focus:ring-[#412a6e]">
                         Read more
                         <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                         </svg>
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
-    )
+    );
 }
