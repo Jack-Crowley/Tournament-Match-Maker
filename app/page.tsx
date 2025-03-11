8c673797d9c5c194850ac4572f4ed903914dc719
Albert Abzalimov <sickloljuly@gmail.com>
2025-03-11 01:26:08 -0400

    logo!

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { faTrophy, faUsers, faChartLine } from '@fortawesome/free-solid-svg-icon
 import { Footer } from "@/components/footer";
 import { createClient } from "@/utils/supabase/client";
 import { useEffect, useState } from "react";
+import Image from "next/legacy/image";
 
 
 export default function Homepage() {
@@ -50,11 +51,15 @@ export default function Homepage() {
               </div>
             </div>
             <div className="lg:w-1/2 lg:pl-12">
-              <div className="bg-gray-200 rounded-lg w-full aspect-video flex items-center justify-center">
-                <svg className="w-24 h-24 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd" />
-                </svg>
-              </div>
+              <Image
+                src={"/tmm_logo.png"}
+                alt={`logo`}
+                width={400}
+                height={400}
+                className="object-cover"
+                priority
+              />
+
             </div>
           </div>
         </div>
